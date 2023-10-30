2083a1e70151f201a773199a2ea2ce14f62a5608
Jon Theis Nilsson <ignorante85@gmail.com>
2023-10-30 07:04:09 +0100

    added crypto

--- client.py
+++ client.py
@@ -4,17 +4,43 @@ import argparse
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 import tkinter
+
+from nacl.public import PrivateKey, Box, PublicKey
+from nacl.utils import EncryptedMessage
+
 title_chat = 'Chatter'
+
 
 def receive():
     """Handles receiving of messages."""
-    global title_chat
+    global title_chat, SERVER_PUBLIC_KEY, BOX
+
+    # exchange keys
+    client_socket.send(CLIENT_PUBLIC_KEY.encode())
+    SERVER_PUBLIC_KEY = PublicKey(client_socket.recv(32))
+    BOX = Box(CLIENT_PRIVATE_KEY, SERVER_PUBLIC_KEY)
+    print(SERVER_PUBLIC_KEY)
+
+
     while True:
         try:
-            msg = client_socket.recv(BUFSIZ).decode("utf8")
-            msg_list.insert(tkinter.END, msg)
-            if msg.startswith('Welcome') and title_chat == 'Chatter':
-                title_chat += ' ' + msg.split()[1]
+            msg = client_socket.recv(BUFSIZ)
+            # print(f"msg received: {msg}")
+
+            #decrypted_msg = decrypt_msg(msg)
+            decrypted_msg = BOX.decrypt(msg).decode()
+            #return decrypted.decode('utf-8')
+
+            # remove name and :
+            # try:
+            #     index = decrypted_msg.index(":")
+            #     decrypted_msg = decrypted_msg[index+1:]
+            # except:
+            #     pass
+
+            msg_list.insert(tkinter.END, decrypted_msg)
+            if decrypted_msg.startswith('Welcome') and title_chat == 'Chatter':
+                title_chat += ' ' + decrypted_msg.split()[1]
                 top.title(title_chat)
         except OSError:  # Possibly client has left the chat.
             break
@@ -23,8 +49,11 @@ def receive():
 def send(event=None):  # event is passed by binders.
     """Handles sending of messages."""
     msg = my_msg.get()
+
+    encrypted_msg = encrypt_msg(msg)
+    print(encrypted_msg)
     my_msg.set("")  # Clears input field.
-    client_socket.send(bytes(msg, "utf8"))
+    client_socket.send(encrypted_msg)
     if msg == "{quit}":
         client_socket.close()
         top.quit()
@@ -34,6 +63,17 @@ def on_closing(event=None):
     """This function is to be called when the window is closed."""
     my_msg.set("{quit}")
     send()
+
+
+def encrypt_msg(msg: str) -> EncryptedMessage:
+    encrypted = BOX.encrypt(bytes(msg.encode()))
+    return encrypted
+
+
+def decrypt_msg(msg: EncryptedMessage) -> str:
+    decrypted = BOX.decrypt(msg.decode())
+    return decrypted.decode('utf-8')
+
 
 top = tkinter.Tk()
 top.title(title_chat)
@@ -58,23 +98,27 @@ send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
-#----Now comes the arguments part----
+# ----Now comes the arguments part----
 parser = argparse.ArgumentParser(description='This is the client for the chat.')
 parser.add_argument('ip', type=str, nargs='?', default='127.0.0.1',
                     help='the ip you want to connect to. (default 127.0.0.1)')
 
-parser.add_argument('-p','--port', type=int, nargs='?', default=33000,
+parser.add_argument('-p', '--port', type=int, nargs='?', default=33000,
                     help='the port. (default 33000)')  
-parser.add_argument('-s','--buff-size', type=int, nargs='?', default=1024,
+parser.add_argument('-s', '--buff-size', type=int, nargs='?', default=1024,
                     help='the size of the buffer. (default 1024)')
                     
 args = parser.parse_args()
-HOST=args.ip
-PORT=args.port 
+HOST = args.ip
+PORT = args.port
 BUFSIZ = args.buff_size
 ADDR = (HOST, PORT)
+CLIENT_PRIVATE_KEY = PrivateKey.generate()
+CLIENT_PUBLIC_KEY = CLIENT_PRIVATE_KEY.public_key
+SERVER_PUBLIC_KEY = None
+BOX: Box = None
 
-#----Now comes the sockets part----
+# ----Now comes the sockets part----
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect(ADDR)
 print(f'[INFO] Connected to {HOST}:{PORT}, buffer size: {BUFSIZ}')
--- server.py
+++ server.py
@@ -4,63 +4,98 @@ import argparse
 from socket import AF_INET, socket, SOCK_STREAM
 from threading import Thread
 
+import nacl.utils
+from nacl.public import PrivateKey, Box, PublicKey, EncryptedMessage
+
 
 def accept_incoming_connections():
     """Sets up handling for incoming clients."""
     while True:
         client, client_address = SERVER.accept()
         print("%s:%s has connected." % client_address)
-        client.send(bytes("Greetings from the cave! Now type your name and press enter!", "utf8"))
+
+        public_keys[client] = PublicKey(client.recv(32))
+        client.send(SERVER_PUBLIC_KEY.encode())
+
+        box = Box(SERVER_PRIVATE_KEY, public_keys[client])
+        msg = b"Greetings from the cave! Now type your name and press enter!"
+        encrypted = box.encrypt(msg)
+        # client.send(encrypted)
+
         addresses[client] = client_address
         Thread(target=handle_client, args=(client,)).start()
 
 
-def handle_client(client):  # Takes client socket as argument.
+def handle_client(client: socket):  # Takes client socket as argument.
     """Handles a single client connection."""
+    box = Box(SERVER_PRIVATE_KEY, public_keys[client])
 
-    name = client.recv(BUFSIZ).decode("utf8")
+    name = box.decrypt(client.recv(BUFSIZ))
+    name = name.decode("utf8")
+
     welcome = 'Welcome %s! If you ever want to quit, type {quit} to exit.' % name
-    client.send(bytes(welcome, "utf8"))
+
+    client.send(encrypt_msg(welcome, client))
     msg = "%s has joined the chat!" % name
-    broadcast(bytes(msg, "utf8"))
+    broadcast(msg)
     clients[client] = name
 
     while True:
         msg = client.recv(BUFSIZ)
-        if msg != bytes("{quit}", "utf8"):
-            broadcast(msg, name+": ")
+        #decrypted_msg = decrypt_msg(msg, public_keys[client])
+
+        decrypted_msg = box.decrypt(msg)
+        decoded_msg = decrypted_msg.decode("utf8")
+
+        if decoded_msg != "{quit}":
+            broadcast(decoded_msg, name + ": ")
         else:
-            client.send(bytes("{quit}", "utf8"))
+            client.send(encrypt_msg("{quit}", client))
             client.close()
             del clients[client]
-            broadcast(bytes("%s has left the chat." % name, "utf8"))
+            # broadcast(bytes("%s has left the chat." % name, "utf8"))
             break
 
 
-def broadcast(msg, prefix=""):  # prefix is for name identification.
+def broadcast(msg: str, prefix=""):  # prefix is for name identification.
     """Broadcasts a message to all the clients."""
 
     for sock in clients:
-        sock.send(bytes(prefix, "utf8")+msg)
-
-        
-clients = {}
-addresses = {}
+        res = prefix + msg
+        encrypted_msg = encrypt_msg(res, sock)
+        sock.send(encrypted_msg)
 
 
-#----Now comes the arguments part----
+def remove_client_public_key(_client: socket):
+    del public_keys[_client]
+
+
+def encrypt_msg(msg: str, _client: socket) -> EncryptedMessage:
+    box = Box(SERVER_PRIVATE_KEY, public_keys[_client])
+    encrypted = box.encrypt(msg.encode('utf-8'))
+    return encrypted
+
+
+clients = {}  # socket, name
+addresses = {}  # socket, client_adress
+public_keys = {}  # socket, PublicKey
+SERVER_PRIVATE_KEY = PrivateKey.generate()
+SERVER_PUBLIC_KEY = SERVER_PRIVATE_KEY.public_key
+
+
+# ----Now comes the arguments part----
 parser = argparse.ArgumentParser(description='This is the server for the chat.')
 parser.add_argument('ip', type=str, nargs='?', default='127.0.0.1',
                     help='the ip you want to bind. (default 127.0.0.1)')
 
-parser.add_argument('-p','--port', type=int, nargs='?', default=33000,
+parser.add_argument('-p', '--port', type=int, nargs='?', default=33000,
                     help='the port. (default 33000)')  
-parser.add_argument('-s','--buff-size', type=int, nargs='?', default=1024,
+parser.add_argument('-s', '--buff-size', type=int, nargs='?', default=1024,
                     help='the size of the buffer. (default 1024)')
                     
 args = parser.parse_args()
-HOST=args.ip
-PORT=args.port 
+HOST = args.ip
+PORT = args.port
 BUFSIZ = args.buff_size
 
 ADDR = (HOST, PORT)
